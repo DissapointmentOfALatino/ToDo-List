953215be2c57b87fa34d96392fde271a1e468e13
Javan Garcia <javan1204@outlook.com>
2025-12-02 13:29:51 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?" />
+          </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What's the task description?" />
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">Add</button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <button>ToDo</button>
+          <button>Completed</button>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
